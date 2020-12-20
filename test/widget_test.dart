5e50caccafdcb25e49b8f3099fc909638a9f2794
nasfos <nasfos84@gmail.com>
2020-12-20 23:47:26 +0800

    update file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:midterm_a201_stiw2044/main.dart';
+import 'package:midterm_a201_stiw2044/mainscreen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
